3760791d7743f73569edb2918f53799d525c5d0b
Dimitris Efstathiou <jimefsta2@gmail.com>
2023-12-03 17:59:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Script for handling Google Maps button click
+document.getElementById('openMaps').addEventListener('click', function() {
+    // Replace the latitude and longitude with your restaurant's location
+    window.open('https://maps.google.com/?q=123 Main Street, Cityville');
+});
